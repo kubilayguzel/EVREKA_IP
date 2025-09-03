11c206764768b36c42db7d6f37a5577aa9b4adc3
kubilayguzel <mkubilayguzel@gmail.com>
2025-09-03 09:17:14 +0300

    Update background.js

--- tp-sorgu-eklentisi-2/background.js
+++ tp-sorgu-eklentisi-2/background.js
@@ -1,9 +1,33 @@
-
+// Web sitenizden gelen mesajları dinle
 chrome.runtime.onMessageExternal.addListener((request, sender, sendResponse) => {
-  if (request && request.type === 'SORGULA_KISI' && request.data) {
-    // Artık burada tabs API kullanılmıyor. Web sayfanız yeni sekmeyi kendisi açmalı:
-    // window.open('https://www.turkpatent.gov.tr/arastirma-yap?form=trademark&kisiNo=XXX&return=ENCODED_URL', '_blank')
-    sendResponse({ status: 'OK', message: 'Lütfen sorgu sekmesini web sayfanızda window.open ile açın.' });
+  if (request.type === 'SORGULA_KISI' && request.data) {
+    const ownerId = request.data;
+    const targetUrl = "https://www.turkpatent.gov.tr/arastirma-yap?form=trademark";
+
+    chrome.tabs.create({ url: targetUrl }, (newTab) => {
+      const listener = (tabId, changeInfo, tab) => {
+        if (tabId === newTab.id && changeInfo.status === 'complete') {
+          chrome.tabs.sendMessage(tabId, {
+            type: 'AUTO_FILL_KISI',
+            data: ownerId
+          });
+          chrome.tabs.onUpdated.removeListener(listener);
+        }
+      };
+      
+      chrome.tabs.onUpdated.addListener(listener);
+    });
+
+    sendResponse({ status: 'OK', message: 'Sorgulama sekmesi oluşturuldu ve yüklenmesi bekleniyor.' });
   }
-  return true;
+  return true; 
+});
+// Tab kapanma durumunda cleanup
+chrome.tabs.onRemoved.addListener((tabId) => {
+    console.log('[TP Eklenti] Tab kapatıldı:', tabId);
+});
+
+// Extension hatası durumunda loglama
+chrome.runtime.onSuspend.addListener(() => {
+    console.log('[TP Eklenti] Extension suspend oldu.');
 });
