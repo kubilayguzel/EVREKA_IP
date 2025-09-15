bdd370e4fded3102b74f1464f555c2a89157aecd
kubilayguzel <mkubilayguzel@gmail.com>
2025-09-15 23:12:31 +0300

    Update background.js

--- tp-sorgu-eklentisi-2/background.js
+++ tp-sorgu-eklentisi-2/background.js
@@ -1,13 +1,15 @@
 // Web sitenizden gelen mesajları dinle
 chrome.runtime.onMessageExternal.addListener((request, sender, sendResponse) => {
-  // Başvuru No (geriye uyum): SORGULA veya SORGULA_BASVURU
+  // Başvuru No için SORGULA veya SORGULA_BASVURU mesajlarını dinle
   if ((request.type === 'SORGULA' || request.type === 'SORGULA_BASVURU') && request.data) {
     const appNo = request.data;
     const targetUrl = "https://www.turkpatent.gov.tr/arastirma-yap?form=trademark";
+    
     chrome.tabs.create({ url: targetUrl }, (newTab) => {
       const listener = (tabId, changeInfo) => {
         if (tabId === newTab.id && changeInfo.status === 'complete') {
-          chrome.tabs.sendMessage(tabId, { type: 'AUTO_FILL', data: appNo });
+          // Başvuru numarası sorgusu için 'AUTO_FILL_BASVURU' mesajını gönder
+          chrome.tabs.sendMessage(tabId, { type: 'AUTO_FILL_BASVURU', data: appNo });
           chrome.tabs.onUpdated.removeListener(listener);
         }
       };
@@ -17,13 +19,15 @@ chrome.runtime.onMessageExternal.addListener((request, sender, sendResponse) => 
     return true;
   }
 
-  // Sahip No: SORGULA_KISI
+  // Sahip No için SORGULA_KISI mesajını dinle
   if (request.type === 'SORGULA_KISI' && request.data) {
     const ownerId = request.data;
     const targetUrl = "https://www.turkpatent.gov.tr/arastirma-yap?form=trademark";
+    
     chrome.tabs.create({ url: targetUrl }, (newTab) => {
       const listener = (tabId, changeInfo) => {
         if (tabId === newTab.id && changeInfo.status === 'complete') {
+          // Sahip numarası sorgusu için 'AUTO_FILL_KISI' mesajını gönder
           chrome.tabs.sendMessage(tabId, { type: 'AUTO_FILL_KISI', data: ownerId });
           chrome.tabs.onUpdated.removeListener(listener);
         }
@@ -33,32 +37,6 @@ chrome.runtime.onMessageExternal.addListener((request, sender, sendResponse) => 
     sendResponse({ status: 'OK', message: 'Sahip No sekmesi açıldı.' });
     return true;
   }
-
-  if (request.type === 'SORGULA' && request.data) {
-    const basvuruNo = request.data;
-    const targetUrl = "https://www.turkpatent.gov.tr/arastirma-yap?form=trademark";
-
-    // Yeni bir sekme oluştur
-    chrome.tabs.create({ url: targetUrl }, (newTab) => {
-      // Bu yeni sekmenin yüklenmesini dinlemek için bir olay dinleyici ekle
-      const listener = (tabId, changeInfo, tab) => {
-        // Eğer güncellenen sekme bizim oluşturduğumuz sekme ise VE yüklenmesi tamamlandıysa
-        if (tabId === newTab.id && changeInfo.status === 'complete') {
-          // Mesajı şimdi, yani sayfa tamamen hazır olduğunda gönder
-          chrome.tabs.sendMessage(tabId, {
-            type: 'AUTO_FILL',
-            data: basvuruNo
-          });
-          // İşi bittiği için bu dinleyiciyi bellekten kaldır
-          chrome.tabs.onUpdated.removeListener(listener);
-        }
-      };
-      
-      // Sekme güncelleme olaylarını dinlemeye başla
-      chrome.tabs.onUpdated.addListener(listener);
-    });
-
-    sendResponse({ status: 'OK', message: 'Sorgulama sekmesi oluşturuldu ve yüklenmesi bekleniyor.' });
-  }
+  
   return true; 
 });
