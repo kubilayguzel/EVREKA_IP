7b74bd4c9a8ad25c18a1c6ce3c5338fe18e3bdfa
kubilayguzel <mkubilayguzel@gmail.com>
2025-12-30 14:34:49 +0300

    Update strategies.js

--- public/js/data-entry/strategies.js
+++ public/js/data-entry/strategies.js
@@ -86,7 +86,7 @@ export class TrademarkStrategy extends BaseStrategy {
             ipType: 'trademark',
             type: 'trademark',
             portfoyStatus: 'active',
-
+            recordOwnerType: getVal('recordOwnerType') || 'self',
             title: brandText,
             brandText: brandText,
             
