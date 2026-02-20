722900426a69403a56932a85fd683df205af368c
kubilayguzel <mkubilayguzel@gmail.com>
2026-02-20 12:49:04 +0300

    Update trademark-similarity-search.js

--- public/js/trademark-similarity-search.js
+++ public/js/trademark-similarity-search.js
@@ -1194,6 +1194,7 @@ const performSearch = async () => {
     const monitoredMarksPayload = filteredMonitoringTrademarks.map(tm => ({
         id: tm.id,
         markName: (tm.title || tm.markName || '').trim() || 'BELİRSİZ_MARKA',
+        searchMarkName: tm.searchMarkName || '', // <--- EKSİK OLAN VE EKLENEN SATIR
         brandTextSearch: tm.brandTextSearch || [], 
         niceClassSearch: tm.niceClassSearch || [],
         goodsAndServicesByClass: tm.goodsAndServicesByClass || []
